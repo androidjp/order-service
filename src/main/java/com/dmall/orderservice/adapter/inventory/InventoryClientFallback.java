958fa90d1523f62ea9dac1254b776858d03981ef
Jasper Wu <jasper.wu@cargosmart.com>
2019-06-13 16:36:54 +0800

    practice for hytrix.

---
+++ src/main/java/com/dmall/orderservice/adapter/inventory/InventoryClientFallback.java
@@ -0,0 +1,22 @@
+package com.dmall.orderservice.adapter.inventory;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Jasper Wu
+ * @date 6/13/2019
+ **/
+@Slf4j
+@Component
+public class InventoryClientFallback implements InventoryClient {
+    @Override
+    public String lock(Lock lock) {
+        return "Unavailable now, check later";
+    }
+
+    @Override
+    public void unlock(String lockId) {
+        log.info("Unavailable now, check later");
+    }
+}
